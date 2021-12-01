f4b535ab09cfe5aaf806c820b88a1aa21759437e
Matiinglant <matiasdaniel1910@gmail.com>
2021-12-01 20:30:58 -0300

    commit incompleto

--- repositorio/bloggreen/apps/posts/views.py
+++ repositorio/bloggreen/apps/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
+from .models import Post, PostView, Like, Comment
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+class PostDeleteView(DeleteView):
+    model = Post
