a10e82feabb0ba3957e8d0ec08ad760e24526539
danamerro <danamerro@gmail.com>
2021-12-01 20:28:26 -0300

    activacion bd y arreglos post

--- repositorio/bloggreen/bloggreen/settings/production.py
+++ repositorio/bloggreen/bloggreen/settings/production.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    '''
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    '''
+
 }
--- repositorio/bloggreen/bloggreen/urls.py
+++ repositorio/bloggreen/bloggreen/urls.py
@@ -3,11 +3,22 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from apps.posts.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', PostListView.as_view(), name='list'),
+    path('create/', PostCreateView.as_view(), name='create'),
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
 ]
-
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
