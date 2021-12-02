0f5f93956922040c0fa8d02476d46cde00e5515a
danamerro <danamerro@gmail.com>
2021-12-02 01:15:24 -0300

    index.css, img, y el index.html

--- repositorio/bloggreen/bloggreen/urls.py
+++ repositorio/bloggreen/bloggreen/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 from apps.posts.views import (
     PostListView,
@@ -13,11 +14,12 @@ from apps.posts.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', PostListView.as_view(), name='list'),
+    path('/list', PostListView.as_view(), name='list'),
     path('create/', PostCreateView.as_view(), name='create'),
     path('<slug>/', PostDetailView.as_view(), name='detail'),
     path('<slug>/update/', PostUpdateView.as_view(), name='update'),
     path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('',views.Inicio,name='inicio')
 ]
 
 if settings.DEBUG:
