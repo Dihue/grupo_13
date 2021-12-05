89aabf4c4d011551eea6488e5475a42f97c36343
Matiinglant <matiasdaniel1910@gmail.com>
2021-12-05 17:39:07 -0300

    login no anda

--- repositorio/bloggreen/apps/users/urls.py
+++ repositorio/bloggreen/apps/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('registro/', views.RegisterUser.as_view(success_url = reverse_lazy('usuarios:registrar_completo')), name='registro'),
+    path('login/', LoginView.as_view(template_name='usuario/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='usuario/logout.html'), name='logout'),
     
 ]
