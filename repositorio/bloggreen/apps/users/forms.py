4a9311ea02261487680038232d5844aabf02cb1e
danamerro <danamerro@gmail.com>
2021-12-06 23:59:26 -0300

    se borraron espacios innecesarios

--- repositorio/bloggreen/apps/users/forms.py
+++ repositorio/bloggreen/apps/users/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm
 from django.forms import fields
 from .models import User
-
 from django import forms
 from django.contrib.auth.models import User
 
